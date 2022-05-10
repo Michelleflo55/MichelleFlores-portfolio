ae642b7d62edc13122fe56fa17f99a3877f9fd37
Michelle Flores <lmfg2456@gmail.com>
2022-05-09 22:00:01 -0700

    added name

---
+++ portfolio/src/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function navbar() {
+    return(
+        <div>
+            <h1>About Me</h1>
+            <h1>Portfolio</h1>
+            <h1>Resume</h1>
+        </div>
+    )
+}
+export default navbar
